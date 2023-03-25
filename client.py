0c8d91c3342510d27337dbe443b73fcdefe6e09e
Brian <b.riverah@uniandes.edu.co>
2023-03-25 17:56:35 -0500

    change

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 5566
@@ -7,22 +8,40 @@ SIZE = 1024
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
 
+def receive_messages(client_socket):
+    print(f"[CLIENT] Waiting for messages...")
+    client_socket.sendall("READY".encode(FORMAT))
+    HASH = client_socket.recv(SIZE).decode(FORMAT)
+    print(f"[CLIENT] HASH recibido: {HASH}")
+    print(f"[CLIENT] ENVIO DE READY")
+    client_socket.close()
+
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
+    # Conexión al servidor
+    client_sockets = []
+    num_clients = int(input("Ingrese el número de clientes: "))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(ADDR)
+    print(f"[KING CLIENT] Connected to server at {IP}:{PORT}")
+    client_socket.sendall(str(num_clients).encode(FORMAT))
+    archivo_transmision = input("Ingrese el nombre del archivo a transmitir: ")
+    print(f"[KING CLIENT] se espera el archivo {archivo_transmision}")
+    archivo_transmision = archivo_transmision.encode(FORMAT)
+    client_socket.sendall(archivo_transmision)
 
-    connected = True
-    while connected:
-        msg = input("> ")
+    for i in range(num_clients):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(ADDR)
+        client_sockets.append(client_socket)
+        print(f"[CLIENT {i}] Connected to server at {IP}:{PORT}")
 
-        client.send(msg.encode(FORMAT))
-
-        if msg == DISCONNECT_MSG:
-            connected = False
-        else:
-            msg = client.recv(SIZE).decode(FORMAT)
-            print(f"[SERVER] {msg}")
-
+    # Recepción de mensajes
+    threads = []
+    for i in range(num_clients):
+        thread = threading.Thread(target=receive_messages, args=(client_sockets[i],))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import wikipedia
+import hashlib
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 5566
@@ -9,34 +10,49 @@ SIZE = 1024
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
 
-def handle_client(conn, addr):
+def handle_client(conn:socket, addr,filename,cantidad_clientes):
     print(f"[NEW CONNECTION] {addr} connected.")
+    listo = conn.recv(SIZE).decode(FORMAT)
+    print(f"[READY][{addr}] {listo}")
+    if listo == "READY":
+        ALLready.append(conn)
+    while len(ALLready) < cantidad_clientes:
+        print(f"[READY][{addr}] Esperando a que todos los clientes esten listos")
+        pass
 
-    connected = True
-    while connected:
-        msg = conn.recv(SIZE).decode(FORMAT)
-        if msg == DISCONNECT_MSG:
-            connected = False
-
-        print(f"[{addr}] {msg}")
-        # msg = f"Msg received: {msg}"
-        msg = wikipedia.summary(msg, sentences=1)
-        conn.send(msg.encode(FORMAT))
-
+    archivo = open(filename, "r")
+    print(f"[ARCHIVO][{addr}] {filename} abierto")
+    hash_archivo = hashlib.md5(archivo.read().encode()).hexdigest()
+    print(f"[ARCHIVO][HASH][{addr}] {filename} leido")
+    conn.sendall(hash_archivo.encode(FORMAT))
+    print(f"[ARCHIVO][HASH][{addr}] {filename} enviado")
     conn.close()
+    
+    
 
 def main():
+    global ALLready
+    global Attended
     print("[STARTING] Server is starting...")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
     server.listen()
     print(f"[LISTENING] Server is listening on {IP}:{PORT}")
-
+    conexion_inicial, addr = server.accept()
+    print(f"[KING CONNECTION] {addr} connected.")
+    cantidad_clientes = int(conexion_inicial.recv(SIZE).decode(FORMAT))
+    print(f"[KING CONNECTION] se esperan {cantidad_clientes} clientes")
+    archivo_transmision = conexion_inicial.recv(SIZE).decode(FORMAT)+".txt"
+    print(f"[KING CONNECTION] espera el archivo {archivo_transmision}")
+    ALLready = []
+    Attended = []
+    
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread = threading.Thread(target=handle_client, args=(conn, addr,archivo_transmision,cantidad_clientes))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
 
 if __name__ == "__main__":
     main()
